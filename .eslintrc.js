3b2e62ced66f25202319764af6b6b78abca91495
Tey García Miranda <tgarciamiranda@gmail.com>
2019-05-07 00:56:37 -0600

    feat/linter
    + eslint integration and configuration for development

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "env": {
+    "es6": true
+  },
+  "rules": {
+      "indent": [2, 2],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ]
+  }
+}
